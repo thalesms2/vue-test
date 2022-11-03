63701df9c40fc39c1a981b4f2f36f65bbb555b87
Thales Minoru Sato <thalesms2@outlook.com>
2022-11-03 17:04:54 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+    import { ref, onMounted } from 'vue'
+
+    const count = ref(0)
+
+    const increment = () => {
+        count.value++
+    }
+
+    onMounted(() => {
+        console.log(`Inicial value is ${count.value}`)
+    })
+</script>
+
+<template>
+    <div>
+        Hello World {{count}}
+        {{ count > 7 ? 'Gol da alemanha' : 'Ainda não!'}}
+        <button @click="increment">Count is {{ count }}</button>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    button {
+        padding: .5em 1em;
+        color: #000020;
+        background-color: #fffeee;
+        border-radius: .5em;
+        margin: .5em;
+        border: none;
+    }
+</style>
